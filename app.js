6f1461c758dd5422301e7d2f49c49882919a2575
Anton Buldenkov <anton@buldenkov.ru>
2022-05-04 09:27:22 +0300

    add loger

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 
 const { celebrate, errors } = require('celebrate');
 
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { limiter } = require('./middlewares/limiter');
 const { auth } = require('./middlewares/auth');
 const { handleError } = require('./middlewares/errors');
@@ -30,6 +31,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(requestLogger);
 app.post('/signin', celebrate({ body: signinSchema }), login);
 app.post('/signup', celebrate({ body: signupSchema }), createUser);
 app.get('/signout', auth, logout);
@@ -38,6 +40,7 @@ app.use('/users', auth, userRoutes);
 app.use('/cards', auth, cardRoutes);
 app.use('*', auth, (req, res, next) => next(new NotFoundError('Страница не найдена')));
 
+app.use(errorLogger);
 app.use(errors());
 app.use(handleError);
 
